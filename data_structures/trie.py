7cd5524089ef28b6930174a7a0562b5e9f6b13b1
Mohammad Zavvari <mz97.mohammad@gmail.com>
2021-04-04 23:17:08 +0430

    Add find_node function + Edit TODO of trie.py

--- data_structures/trie.py
+++ data_structures/trie.py
@@ -5,7 +5,10 @@ class Node:
         self.next_alphabet = [None for i in range(num_of_alphabets)]
 
 
-def alphabet_to_index(character):
+def alphabet_to_index(character) -> int:
+    asci_char = ord(character)
+    if asci_char > ord('z') or asci_char < ord('A'):
+        raise Exception("Word cannot have things other than letters.")
     return (ord(character) - 65) % 32  # ord('A') = 65
 
 
@@ -17,7 +20,6 @@ class Trie:
         pass
 
     def insert(self, word):
-        # length = len(word)
         node = self.root
         for character in word:
             index = alphabet_to_index(character)
@@ -26,3 +28,37 @@ class Trie:
             node = node.next_alphabet[index]
         node.mark = True
         return
+
+    def find(self, word):
+        node = self.root
+        for character in word:
+            index = alphabet_to_index(character)
+            if node.next_alphabet[index] is None:
+                return False
+            node = node.next_alphabet[index]
+        return node.mark
+
+    def find_node(self, word):
+        node = self.root
+        for character in word:
+            index = alphabet_to_index(character)
+            if node.next_alphabet[index] is None:
+                return None
+            node = node.next_alphabet[index]
+        return node
+
+    def autocomplete(self, incomplete_word):
+
+        # TODO: Find the uncompleted node in case of existence.
+        # TODO: Find completed words with DFS.
+
+        if self.find_node(incomplete_word) is None:
+            return []
+
+
+
+        node = self.root
+        for character in incomplete_word:
+            pass  # TODO: Do the DFS JOB
+
+    def dfs
